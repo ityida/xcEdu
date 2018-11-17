5fdb73c92d5caa39e4fd7a0e224a7efac14f5bbb
yida <zhangyfei126@yeah.com>
2018-11-17 20:34:54 +0800

    【YES】2018.11.17 rabbitMq 测试 【DEC】接口定义

---
+++ test-rabbitmq-consumer/src/main/java/com/xuecheng/test/rabbitmq/TestRabbitmqApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng.test.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Describle:
+ * @Author:zhangyifei
+ * @Date:2018/11/17
+ */
+@SpringBootApplication
+public class TestRabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRabbitmqApplication.class,args);
+    }
+}
